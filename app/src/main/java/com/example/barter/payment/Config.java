7d115fd43755b85a61eda6c20397bb2ceb9b383a
lester24-dev <lestergime24@gmail.com>
2024-06-30 21:28:27 +0800

    Revert "Add Files 2"
    This reverts commit 779968bf23a152c587ed467b83b88c970b0b540d.

--- app/src/main/java/com/example/barter/payment/Config.java
+++
@@ -1,6 +0,0 @@
-package com.example.barter.payment;
-
-public class Config {
-
-    public static final String PAYMENT_CLIENT_ID = "AVHztDLyZbQcxrviwNc_W-qpGD_1lUI8DYGCdNf7uF5KeSlEBJCNcJLghTGl9PsEZZXH4_mKvb5gL-V5";
-}
